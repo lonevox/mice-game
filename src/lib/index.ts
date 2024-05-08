4130c92c3c1ab8c8ba7db3e95aca76d712ac6138
lonevox <quinn.epicness@gmail.com>
2024-05-08 23:36:57 +1200

    Initial layout and resources GUI

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,1 +1,39 @@
-// place files you want to import through the `$lib` alias in this folder.
+export type Rarity = {
+	name: string,
+	color: string,
+}
+export const Rarities: Record<string, Rarity> = {
+	Common: {
+		name: "Common",
+		color: "white",
+	},
+	Uncommon: {
+		name: "Uncommon",
+		color: "green",
+	}
+}
+
+export type Category = {
+	name: string,
+	open: boolean,
+}
+export const categoryDefaults = {
+	open: false,
+}
+
+export type Resource = {
+	name: string,
+	rarity: Rarity,
+	category?: Category,
+	amount: number,
+	maxAmount: number,
+	production: number,
+	productionRatio: number,
+}
+export const resourceDefaults = {
+	rarity: Rarities.Common,
+	amount: 0,
+	maxAmount: 100,
+	production: 0,
+	productionRatio: 0,
+}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,15 +1,50 @@
-<!-- YOU CAN DELETE EVERYTHING IN THIS PAGE -->
+<script lang="ts">
+	import { Rarities, type Resource, resourceDefaults } from '$lib';
 
-<div class="container h-full mx-auto flex justify-center items-center">
-	<div class="space-y-5">
-		<h1 class="h1">Let's get cracking bones!</h1>
-		<p>Start by exploring:</p>
-		<ul>
-			<li><code class="code">/src/routes/+layout.svelte</code> - barebones layout</li>
-			<li><code class="code">/src/app.postcss</code> - app wide css</li>
-			<li>
-				<code class="code">/src/routes/+page.svelte</code> - this page, you can replace the contents
-			</li>
-		</ul>
+	const resources = $state<Resource[]>([])
+	resources.push({
+		...resourceDefaults,
+		name: "thing",
+		rarity: Rarities.Common,
+		production: 0.125,
+	})
+	resources.push({
+		...resourceDefaults,
+		name: "another longer thing",
+		rarity: Rarities.Uncommon,
+	})
+
+	const ticksPerSecond = 5;
+
+	function formatProduction(production: number): string {
+		if (production > 0) {
+			return "+" + production * ticksPerSecond + "/s";
+		}
+		if (production < 0) {
+			return "-" + production * ticksPerSecond + "/s";
+		}
+		return "";
+	}
+</script>
+
+<div class="flex flex-row max-w-screen-2xl mx-auto space-x-8">
+	<div class="basis-1/4 space-y-1">
+		<h4 class="h4">Resources</h4>
+		<div class="mx-auto bg-surface-900 table w-96 border-spacing-1">
+			{#each resources as resource}
+				<div class="table-row hover:bg-surface-800">
+					<p class="table-cell max-w-16 px-1 truncate" style="color: {resource.rarity.color}">{resource.name}</p>
+					<p class="table-cell px-1">{resource.amount}</p>
+					<p class="table-cell px-1 text-surface-500">/{resource.maxAmount}</p>
+					<p class="table-cell px-1">{formatProduction(resource.production)}</p>
+				</div>
+			{/each}
+		</div>
+	</div>
+	<div class="basis-1/2">
+		<h4 class="h4">Main Content</h4>
+	</div>
+	<div class="basis-1/4">
+		<h4 class="h4">Log</h4>
 	</div>
 </div>
