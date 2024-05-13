8c87548f4d58eb96033da34556a8fd9bf70aace0
lonevox <quinn.epicness@gmail.com>
2024-05-14 00:43:49 +1200

    Simplify Building binding

--- src/lib/components/LocationTab.svelte
+++ src/lib/components/LocationTab.svelte
@@ -6,8 +6,8 @@
 {#each Object.values(locations) as location}
 	<h4 class="h4">{location.name}</h4>
 	<div class="grid grid-cols-2 gap-2">
-		{#each location.buildings as building, i}
-			<Building bind:building={location.buildings[i]} />
+		{#each location.buildings as building}
+			<Building bind:building />
 		{/each}
 	</div>
 {/each}
