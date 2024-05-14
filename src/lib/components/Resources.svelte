9992f73da1e31800a4d4f2bcd500bb4f3f1d94a6
lonevox <quinn.epicness@gmail.com>
2024-05-14 15:52:25 +1200

    Added tooltips for resource names
    This allows you to see what the full name of a resource is when it is obscured in the resource table by hovering over the text.

--- src/lib/components/Resources.svelte
+++ src/lib/components/Resources.svelte
@@ -8,7 +8,7 @@
 	<div class="mx-auto bg-surface-900 table w-96 border-spacing-1">
 		{#each Object.values(resources) as resource}
 			<div class="table-row hover:bg-surface-800">
-				<div class="table-cell px-1 max-w-16 truncate" style="color: {resource.rarity.color}">{resource.name.toLowerCase()}</div>
+				<div class="table-cell px-1 max-w-16 truncate" style="color: {resource.rarity.color}" title={resource.name.toLowerCase()}>{resource.name.toLowerCase()}</div>
 				<div class="table-cell px-1 min-w-10">{formatDecimal(resource.amount)}</div>
 				<div class="table-cell px-1 min-w-10 text-surface-500">/{resource.maxAmount}</div>
 				<div class="table-cell px-1 min-w-10">{formatProduction(resource.production.value)}</div>
