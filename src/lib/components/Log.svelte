73f560475e86762007adbfe43e1d2058b707f3af
lonevox <quinn.epicness@gmail.com>
2024-05-22 21:25:35 +1200

    Change text in log

--- src/lib/components/Log.svelte
+++ src/lib/components/Log.svelte
@@ -4,7 +4,7 @@
 
 <div>
 	<h4 class="h4">Log</h4>
-	<p class="italic text-surface-700-200-token">You are a mouse in a clearing of grain.</p>
+	<p class="italic text-surface-700-200-token">You are a mouse in a field of grain.</p>
 	<hr />
 	<button class="text-surface-500-400-token" onclick={() => logs.length = 0}>Clear log</button>
 	{#each logs as log}
