508750e7ef418ce7c5e1ac12a150c17472fb0435
lonevox <quinn.epicness@gmail.com>
2026-09-11 20:20:38 +1200

    Update eslint config to eslint 9
    Yeah, eslint was broken before because I kept the old config.

---
+++ eslint.config.js
@@ -0,0 +1,49 @@
+import js from '@eslint/js';
+import tsPlugin from '@typescript-eslint/eslint-plugin';
+import tsParser from '@typescript-eslint/parser';
+import prettier from 'eslint-config-prettier';
+import sveltePlugin from 'eslint-plugin-svelte';
+import globals from 'globals';
+
+export default [
+	{
+		ignores: [
+			'.svelte-kit/**',
+			'build/**',
+			'node_modules/**',
+			'package/**',
+			'*.config.js.timestamp-*',
+			'*.config.ts.timestamp-*'
+		]
+	},
+	js.configs.recommended,
+	{
+		languageOptions: { globals: { ...globals.browser, ...globals.node } }
+	},
+	{
+		files: ['**/*.ts', '**/*.svelte'],
+		plugins: { '@typescript-eslint': tsPlugin },
+		languageOptions: {
+			parser: tsParser,
+			parserOptions: { sourceType: 'module', ecmaVersion: 2020 },
+			globals: { ...globals.browser, ...globals.node }
+		},
+		rules: tsPlugin.configs.recommended.rules
+	},
+	...sveltePlugin.configs['flat/recommended'],
+	{
+		files: ['**/*.svelte'],
+		languageOptions: {
+			parserOptions: { parser: tsParser }
+		}
+	},
+	{
+		files: ['**/*.svelte.ts', '**/*.svelte.js'],
+		languageOptions: {
+			parser: tsParser,
+			globals: { $state: 'readonly', $derived: 'readonly' }
+		},
+		rules: { 'svelte/prefer-svelte-reactivity': 'off' }
+	},
+	prettier
+];
